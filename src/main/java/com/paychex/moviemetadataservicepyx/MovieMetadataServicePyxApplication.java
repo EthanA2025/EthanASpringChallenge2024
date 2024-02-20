753d3441518d9cf32147b4fdb6cd40a7ad77f80e
EthanA2025 <eja9979@g.rit.edu>
2024-02-20 13:43:30 -0500

    feat: main functions

--- src/main/java/com/paychex/moviemetadataservicepyx/MovieMetadataServicePyxApplication.java
+++ src/main/java/com/paychex/moviemetadataservicepyx/MovieMetadataServicePyxApplication.java
@@ -1,10 +1,16 @@
 package com.paychex.moviemetadataservicepyx;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class MovieMetadataServicePyxApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieMetadataServicePyxApplication.class, args);
--- src/test/java/com/paychex/moviemetadataservicepyx/MovieMetadataServicePyxApplicationTests.java
+++ src/test/java/com/paychex/moviemetadataservicepyx/MovieMetadataServicePyxApplicationTests.java
@@ -1,13 +1,38 @@
 package com.paychex.moviemetadataservicepyx;
 
+import com.paychex.moviemetadataservicepyx.controller.MovieController;
+import com.paychex.moviemetadataservicepyx.model.Movie;
+import com.paychex.moviemetadataservicepyx.repository.MovieRepository;
+import com.paychex.moviemetadataservicepyx.utility.TitleCaseConverter;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MovieMetadataServicePyxApplicationTests {
 
+	@Autowired
+	private MovieController controller;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(controller);
 	}
 
+	@Test
+	void convertToTitleCase() {
+		List<Movie> movies = controller.getMoviesByTitle("How to Train Your Dragon");
+		String expected = "How To Train Your Dragon";
+		String actual = movies.get(0).getTitle();
+
+		assertEquals(expected, actual);
+	}
 }
