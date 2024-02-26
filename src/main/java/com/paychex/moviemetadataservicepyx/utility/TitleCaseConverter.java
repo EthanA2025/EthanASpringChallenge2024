4edc4559cded91189753a4df42d5823d5270e97f
EthanA2025 <eja9979@g.rit.edu>
2024-02-26 09:10:51 -0500

    feat: Split up titleCase methods for better readability

--- src/main/java/com/paychex/moviemetadataservicepyx/utility/TitleCaseConverter.java
+++ src/main/java/com/paychex/moviemetadataservicepyx/utility/TitleCaseConverter.java
@@ -1,29 +1,33 @@
 package com.paychex.moviemetadataservicepyx.utility;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class TitleCaseConverter {
     private static int index = 0;
     public TitleCaseConverter() {}
-    public static String titleCase(String in) {
+    public static String conversion(@NotNull String word) {
+        String conversion = "";
+        while(index < word.length()) {
+            if(Character.isLetter(word.charAt(index))) {
+                conversion += word.substring(index,index+1).toUpperCase() + word.substring(index+1).toLowerCase();
+                index = 0;
+                return conversion;
+            } else {
+                conversion += word.charAt(index);
+                index++;
+            }
+        }
+        index = 0;
+        return word;
+    }
+
+    public static String titleCase(@NotNull String in) {
         index = 0;
         return Arrays.stream(in.split(" "))
-                .map(word ->  {
-                    String conversion = "";
-                    while(index < word.length()) {
-                        if(Character.isLetter(word.charAt(index))) {
-                            conversion += word.substring(index,index+1).toUpperCase() + word.substring(index+1).toLowerCase();
-                            index = 0;
-                            return conversion;
-                        } else {
-                            conversion += word.charAt(index);
-                            index++;
-                        }
-                    }
-                    index = 0;
-                    return word;
-                })
+                .map(TitleCaseConverter::conversion)
                 .collect(Collectors.joining(" "));
     }
 
